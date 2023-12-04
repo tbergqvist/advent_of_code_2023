275842ebfb82c4aaab86d15eb8cce5ffdec6974c
tbergqvist <tim@piktiv.se>
2023-12-04 21:02:59 +0100

    reduce copy paste

--- src/day_3.rs
+++ src/day_3.rs
@@ -3,20 +3,19 @@ use std::collections::HashMap;
 use itertools::Itertools;
 use regex::Regex;
 
-fn is_special(input: &Vec<&str>, x: i32, y: i32) -> bool {
-	let line_length = input[0].len() as i32;
-	if x >= 0 && y >= 0 && x < line_length && y < input.len() as i32 {
-		let bytes = input[y as usize].as_bytes();
-		let c = bytes[x as usize] as char;
-		return c != '.' && !c.is_digit(10);
+fn special_char_around<F>(input: &Vec<&str>, x: i32, y: i32, length: i32, is_special: F) -> Vec<(i32, i32)> where F: Fn(char)->bool {
+	let mut neighbor_pos: Vec<(i32, i32)> = vec![(x - 1, y), (x + length, y)];
+	for i in -1..length + 1 {
+		neighbor_pos.push((x + i, y - 1));	
+		neighbor_pos.push((x + i, y + 1));	
 	}
-	false
-}
 
-fn special_char_around(input: &Vec<&str>, x: i32, y: i32, length: i32) -> bool {
-	is_special(input, x - 1, y) || 
-	is_special(input, x + length, y) || 
-	(-1..length + 1 as i32).into_iter().any(|pos| is_special(input, x + pos, y - 1) || is_special(input, x + pos, y + 1))
+	let max_x = input[0].len() as i32;
+	let max_y = input.len() as i32;
+	neighbor_pos.into_iter()
+		.filter(|(x, y)| *x >= 0 && *y >= 0 && *x < max_x && *y < max_y)
+		.filter(|(x, y)| is_special(input[*y as usize].as_bytes()[*x as usize] as char))
+		.collect_vec()
 }
 
 pub fn a(input: &str) -> i32 {
@@ -26,31 +25,10 @@ pub fn a(input: &str) -> i32 {
 		.enumerate()
 		.flat_map(|(i, row)|re.find_iter(row).map(move |m|(i, m)))
 		.filter(|(i, m)| {
-			special_char_around(&positions, m.start() as i32, *i as i32, m.len() as i32)
+			!special_char_around(&positions, m.start() as i32, *i as i32, m.len() as i32, |c| c != '.' && !c.is_digit(10)).is_empty()
 		})
 		.map(|(_, m)|m.as_str().parse::<i32>().unwrap())
 		.sum()
-}
-
-fn is_special2(input: &Vec<&str>, x: i32, y: i32) -> Option<(i32, i32)> {
-	let line_length = input[0].len() as i32;
-	if x >= 0 && y >= 0 && x < line_length && y < input.len() as i32 {
-		let bytes = input[y as usize].as_bytes();
-		let c = bytes[x as usize] as char;
-		return if c == '*' {Some((x, y))} else { None };
-	}
-	None
-}
-
-fn special_char_around2(input: &Vec<&str>, x: i32, y: i32, length: i32) -> Vec<(i32, i32)> {
-	let mut neighbors: Vec<Option<(i32, i32)>> = vec![];
-	neighbors.push(is_special2(input, x - 1, y));
-	neighbors.push(is_special2(input, x + length, y));
-	neighbors.append(&mut (-1..length + 1 as i32).into_iter().map(|pos| is_special2(input, x + pos, y - 1)).collect_vec());
-	neighbors.append(&mut (-1..length + 1 as i32).into_iter().map(|pos| is_special2(input, x + pos, y + 1)).collect_vec());
-	neighbors.into_iter()
-		.filter_map(|o|o)
-		.collect_vec()
 }
 
 pub fn b(input: &str) -> i32 {
@@ -61,7 +39,7 @@ pub fn b(input: &str) -> i32 {
 		.flat_map(|(i, row)|re.find_iter(row).map(move |m|(i, m)))
 		.flat_map(|(i, m)| {
 			let num = m.as_str().parse::<i32>().unwrap();
-			special_char_around2(&positions, m.start() as i32, i as i32, m.len() as i32)
+			special_char_around(&positions, m.start() as i32, i as i32, m.len() as i32, |c| c == '*')
 				.into_iter()
 				.map(move |l|(l, num))
 		})
