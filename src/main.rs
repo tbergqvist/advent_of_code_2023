89022549c2c1fd7e708ff8b72bb3371aa3ed861c
tbergqvist <tim@piktiv.se>
2023-12-06 20:36:29 +0100

    added more time stuff

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
 use std::fs;
 use paste::paste;
 use advent_of_code_2023::*;
-
+use std::time::Instant;
 
 macro_rules! run_days {
 	( $( $x:expr), + ) => {
 		$(
 			{
 				paste!{
-					let input = fs::read_to_string(
-						concat!("./inputs/", stringify!($x), ".txt")
-					).unwrap();
-
-					println!(concat!($x, "a:{}"), [<day _ $x>]::a(&input));
-					println!(concat!($x, "b:{}"), [<day _ $x>]::b(&input));
+					let input = fs::read_to_string(concat!("./inputs/", stringify!($x), ".txt")).unwrap();
+					let before = Instant::now();
+					println!(concat!($x, "a: {}, time: {:?}"), [<day _ $x>]::a(&input), before.elapsed());
+					let beforeb = Instant::now();
+					println!(concat!($x, "b: {}, time: {:?}"), [<day _ $x>]::b(&input), beforeb.elapsed());
 				}
 			}
 		)*
