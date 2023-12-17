ecad279885dfce89ba81a5ebfd0a2b9a098c54e7
tbergqvist <tim@piktiv.se>
2023-12-17 15:42:26 +0100

    added day 15 part 2

--- src/day_15.rs
+++ src/day_15.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 fn hash(s: &str) -> usize {
 	s.bytes().fold(0, |sum, byte| {
 		(sum + byte as usize) * 17 % 256
@@ -12,5 +14,32 @@ pub fn a(input: &str) -> usize {
 }
 
 pub fn b(input: &str) -> usize {
-	0
+	let mut boxes = vec![Vec::new();256];
+	for command in input.split(",") {
+		let command_label: String = command.chars().take_while(|b| *b != '-' && *b != '=').collect();
+		let command_nr = hash(&command_label);
+		if command.ends_with('-') {
+			if let Some((i, _)) = boxes[command_nr].iter().find_position(|(key, _)| *key == command_label) {
+				boxes[command_nr].remove(i);
+			}
+		} else {
+			let focal = command.split("=").last().map(|f|f.parse::<usize>().unwrap()).unwrap();
+			if let Some(existing) = boxes[command_nr].iter_mut().find(|(key, _)| *key == command_label) {
+				existing.1 = focal;
+			} else {
+				boxes[command_nr].push((command_label.clone(), focal));
+			}
+		}
+	}
+
+	boxes.into_iter()
+		.zip(1..)
+		.flat_map(|(box_vec, box_nr)| {
+			box_vec.into_iter()
+				.zip(1..)
+				.map(move |((_, focal_point), slot_nr)| {
+					box_nr * slot_nr * focal_point
+				})
+		})
+		.sum()
 }
